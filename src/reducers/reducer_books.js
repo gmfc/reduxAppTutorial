768478320028d56334a508d5389fdafb15560a3d
gmfc <gabriel.mfcorreia@gmail.com>
2018-10-05 18:45:33 -0300

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Javascript, the good parts' },
+    { title: 'HP' },
+    { title: 'Lista telefonica' },
+    { title: 'Javascript, the bad parts' }
+  ]
+}
